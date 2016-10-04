362e586949e710e1faeb89b888d4773a58bdbc1c
Dario Magliocchetti <darioml1911@gmail.com>
2016-10-05 00:41:42 +0100

    backend updates

--- api/quickstart/models.py
+++ api/quickstart/models.py
@@ -4,9 +4,23 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birthday = models.DateField(null=True)
+    phone_number = models.CharField(max_length=100, null=True)
     image_name = models.CharField(max_length=255, blank=True)
+    child_contact = models.ManyToManyField("Contact")
+    notes = models.TextField(null=True)
+
+    def displayName(self):
+        return u'%s, %s' % (self.last_name, self.first_name)
 
     def __unicode__(self):
-        return u'%s' % (self.name)
+        return self.displayName()
+
+
+# class Notes(models.Model):
+#     date
+#     location
+#     content
+#
--- api/quickstart/serializers.py
+++ api/quickstart/serializers.py
@@ -14,14 +14,6 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ('url', 'name')
 
-class ContactSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
-    phone_number = serializers.CharField(max_length=100)
-    image_name = serializers.CharField(max_length=255, allow_blank=True, required=False)
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Contact.objects.create(**validated_data)
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
